0c5b23741ac1664e25a4342a50c6e43ca256dfbc
WinstonMDP <skrimer.swat@bk.ru>
2024-11-19 12:49:05 +0300

    Refactor

--- src/systemd.c
+++ src/systemd.c
@@ -1,17 +1,88 @@
 #include "collectd.h"
 
+#include "metric.h"
 #include "plugin.h"
-#include "utils/common/common.h"
 
 #include "systemd/sd-bus.h"
+#include "utils/common/common.h"
 
-int get_prop(sd_bus *bus, char const type[static 1], char const prop[static 1],
-             void *var) {
+typedef struct systemd_accounting_group systemd_accounting_group;
+struct systemd_accounting_group {
+  char const *accounting_flag;
+  char **metrics;
+};
+
+systemd_accounting_group const groups[] = {
+    {
+        .accounting_flag = "MemoryAccounting",
+        .metrics =
+            (char *[]){
+                "MemoryAvailable",
+                "MemoryCurrent",
+                "MemoryPeak",
+                "MemorySwapCurrent",
+                "MemoryZSwapCurrent",
+                "MemorySwapPeak",
+                NULL,
+            },
+    },
+    {
+        .accounting_flag = "IOAccounting",
+        .metrics =
+            (char *[]){
+                "IOReadBytes",
+                "IOReadOperations",
+                "IOWriteBytes",
+                "IOWriteOperations",
+                NULL,
+            },
+    },
+    {
+        .accounting_flag = "CPUAccounting",
+        .metrics =
+            (char *[]){
+                "CPUUsageNSec",
+                NULL,
+            },
+    },
+    {
+        .accounting_flag = "IPAccounting",
+        .metrics =
+            (char *[]){
+                "IPEgressBytes",
+                "IPEgressPackets",
+                "IPIngressBytes",
+                "IPIngressPackets",
+                NULL,
+            },
+    },
+    {
+        .accounting_flag = "TasksAccounting",
+        .metrics =
+            (char *[]){
+                "TasksCurrent",
+                NULL,
+            },
+    },
+    {
+        .accounting_flag = "true",
+        .metrics =
+            (char *[]){
+                "NRestarts",
+                NULL,
+            },
+    },
+    {.accounting_flag = NULL},
+};
+
+// Err: negative
+static int get_prop(sd_bus *bus, char const type[static 1],
+                    char const prop[static 1], void *var, sd_bus_error *err) {
   sd_bus_message *m = NULL;
   int r = sd_bus_get_property(
       bus, "org.freedesktop.systemd1",
-      "/org/freedesktop/systemd1/unit/chronyd_2eservice",
-      "org.freedesktop.systemd1.Service", prop, NULL, &m, type);
+      "/org/freedesktop/systemd1/unit/avahi_2ddaemon_2eservice",
+      "org.freedesktop.systemd1.Service", prop, err, &m, type);
   if (r < 0) {
     return r;
   }
@@ -20,118 +91,54 @@ int get_prop(sd_bus *bus, char const type[static 1], char const prop[static 1],
   return r;
 }
 
-int submit(counter_t val, char const type_instance[static 1]) {
-  value_list_t vl = VALUE_LIST_INIT;
-  value_t values[1] = {{.counter = val}};
-  vl.values = values;
-  vl.values_len = 1;
-  strncpy(vl.plugin, "systemd", sizeof(vl.plugin));
-  strncpy(vl.type, "systemd_type", sizeof(vl.type));
-  strncpy(vl.type_instance, type_instance, sizeof(vl.type_instance) - 1);
-  return plugin_dispatch_values(&vl);
-}
-
-int submit_block(sd_bus *bus, char const accounting[static 1], size_t nprops,
-                 char const *props[static nprops]) {
-  bool accounting_var;
-  int r = get_prop(bus, "b", accounting, &accounting_var);
-  if (r < 0) {
-    return r;
-  }
-
-  if (accounting_var) {
-    for (size_t i = 0; i < nprops; ++i) {
-      uint64_t value;
-      get_prop(bus, "t", props[i], &value);
-      if (r < 0) {
-        // ERROR("Failed to get CPUUsageNSec property: %s\n", strerror(-r));
-        return r;
-      }
-
-      r = submit(value, props[i]);
-      if (r != 0) {
-        // ERROR("Failed to dispatch: %s\n", STRERROR(r));
-        return r;
-      }
-    }
-  }
-  return 0;
-}
-
 static int systemd_read(void) {
   sd_bus *bus = NULL;
-
+  sd_bus_error sd_bus_err = SD_BUS_ERROR_NULL;
   int r = sd_bus_open_system(&bus);
   if (r < 0) {
-    ERROR("Failed to connect to system bus: %s\n", strerror(-r));
+    ERROR("Failed to connect to system bus: %s", strerror(-r));
     goto defer;
   }
+  for (systemd_accounting_group const *groups_it = groups;
+       groups_it->accounting_flag != NULL; ++groups_it) {
+    bool accounting_flag_var = true;
+    if (strcmp(groups_it->accounting_flag, "true")) {
+      r = get_prop(bus, "b", groups_it->accounting_flag, &accounting_flag_var,
+                   &sd_bus_err);
+      if (r < 0) {
+        ERROR("Failed to get %s accounting flag: %s {%s}, %s",
+              groups_it->accounting_flag, sd_bus_err.name, sd_bus_err.message,
+              strerror(-r));
+        goto defer;
+      }
+    }
+    if (accounting_flag_var) {
+      for (char **metrics_it = groups_it->metrics; *metrics_it != NULL;
+           ++metrics_it) {
+        uint64_t val;
+        r = get_prop(bus, "t", *metrics_it, &val, &sd_bus_err);
+        if (r < 0) {
+          ERROR("Failed to get %s property: %s {%s}, %s", *metrics_it,
+                sd_bus_err.name, sd_bus_err.message, strerror(-r));
+          goto defer;
+        }
 
-  r = submit_block(bus, "CPUAccounting", 1, (char const *[]){"CPUUsageNSec"});
-  if (r != 0) {
-    ERROR("Failed to submit CPU block: %s", STRERROR(r));
-    goto defer;
-  }
-
-  r = submit_block(bus, "MemoryAccounting", 6,
-                   (char const *[]){
-                       "MemoryAvailable",
-                       "MemoryCurrent",
-                       "MemoryPeak",
-                       "MemorySwapCurrent",
-                       "MemoryZSwapCurrent",
-                       "MemorySwapPeak",
-                   });
-  if (r != 0) {
-    ERROR("Failed to submit Memory block: %s", STRERROR(r));
-    goto defer;
-  }
-
-  r = submit_block(bus, "IOAccounting", 4,
-                   (char const *[]){
-                       "IOReadBytes",
-                       "IOReadOperations",
-                       "IOWriteBytes",
-                       "IOWriteOperations",
-                   });
-  if (r != 0) {
-    ERROR("Failed to submit IO block: %s", STRERROR(r));
-    goto defer;
-  }
-
-  r = submit_block(bus, "IPAccounting", 4,
-                   (char const *[]){
-                       "IPEgressBytes",
-                       "IPEgressPackets",
-                       "IPIngressBytes",
-                       "IPIngressPackets",
-                   });
-  if (r != 0) {
-    ERROR("Failed to submit IO block: %s", STRERROR(r));
-    goto defer;
-  }
-
-  r = submit_block(bus, "TasksAccounting", 1, (char const *[]){"TasksCurrent"});
-  if (r != 0) {
-    ERROR("Failed to submit IO block: %s", STRERROR(r));
-    goto defer;
-  }
-
-  uint64_t value;
-  get_prop(bus, "t", "NRestarts", &value);
-  if (r < 0) {
-    ERROR("Failed to get CPUUsageNSec property: %s\n", strerror(-r));
-    goto defer;
-  }
-
-  r = submit(value, "NRestarts");
-  if (r != 0) {
-    ERROR("Failed to dispatch: %s\n", STRERROR(r));
-    goto defer;
+        metric_family_t fam = {.name = *metrics_it,
+                               .type = METRIC_TYPE_COUNTER};
+        metric_family_metric_append(&fam, (metric_t){.value.counter = val});
+        r = plugin_dispatch_metric_family(&fam);
+        metric_family_metric_reset(&fam);
+        if (r != 0) {
+          ERROR("Failed to dispatch: %s", STRERROR(r));
+          goto defer;
+        }
+      }
+    }
   }
 
 defer:
   sd_bus_unref(bus);
+  sd_bus_error_free(&sd_bus_err);
 
   return EXIT_SUCCESS;
 }
