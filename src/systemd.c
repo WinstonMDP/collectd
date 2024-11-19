dc561dcc41a1c3690255bdcbed68e344772549aa
WinstonMDP <skrimer.swat@bk.ru>
2024-11-19 14:15:00 +0300

    Fix systemd bus open

--- src/systemd.c
+++ src/systemd.c
@@ -6,74 +6,82 @@
 #include "systemd/sd-bus.h"
 #include "utils/common/common.h"
 
+typedef struct systemd_metric systemd_metric;
+struct systemd_metric {
+  char *name;
+  char const *type;
+};
+
 typedef struct systemd_accounting_group systemd_accounting_group;
 struct systemd_accounting_group {
   char const *accounting_flag;
-  char **metrics;
+  systemd_metric *metrics;
 };
 
 systemd_accounting_group const groups[] = {
     {
         .accounting_flag = "MemoryAccounting",
         .metrics =
-            (char *[]){
-                "MemoryAvailable",
-                "MemoryCurrent",
-                "MemoryPeak",
-                "MemorySwapCurrent",
-                "MemoryZSwapCurrent",
-                "MemorySwapPeak",
-                NULL,
+            (systemd_metric[]){
+                {.name = "MemoryAvailable", .type = "t"},
+                {.name = "MemoryCurrent", .type = "t"},
+                {.name = "MemoryPeak", .type = "t"},
+                {.name = "MemorySwapCurrent", .type = "t"},
+                {.name = "MemoryZSwapCurrent", .type = "t"},
+                {.name = "MemorySwapPeak", .type = "t"},
+                {.name = NULL},
             },
     },
     {
         .accounting_flag = "IOAccounting",
         .metrics =
-            (char *[]){
-                "IOReadBytes",
-                "IOReadOperations",
-                "IOWriteBytes",
-                "IOWriteOperations",
-                NULL,
+            (systemd_metric[]){
+                {.name = "IOReadBytes", .type = "t"},
+                {.name = "IOReadOperations", .type = "t"},
+                {.name = "IOWriteBytes", .type = "t"},
+                {.name = "IOWriteOperations", .type = "t"},
+                {.name = NULL},
             },
     },
     {
         .accounting_flag = "CPUAccounting",
         .metrics =
-            (char *[]){
-                "CPUUsageNSec",
-                NULL,
+            (systemd_metric[]){
+                {.name = "CPUUsageNSec", .type = "t"},
+                {.name = NULL},
             },
     },
     {
         .accounting_flag = "IPAccounting",
         .metrics =
-            (char *[]){
-                "IPEgressBytes",
-                "IPEgressPackets",
-                "IPIngressBytes",
-                "IPIngressPackets",
-                NULL,
+            (systemd_metric[]){
+                {.name = "IPEgressBytes", .type = "t"},
+                {.name = "IPEgressPackets", .type = "t"},
+                {.name = "IPIngressBytes", .type = "t"},
+                {.name = "IPIngressPackets", .type = "t"},
+                {.name = NULL},
             },
     },
     {
         .accounting_flag = "TasksAccounting",
         .metrics =
-            (char *[]){
-                "TasksCurrent",
-                NULL,
+            (systemd_metric[]){
+                {.name = "TasksCurrent", .type = "t"},
+                {.name = NULL},
             },
     },
     {
         .accounting_flag = "true",
         .metrics =
-            (char *[]){
-                "NRestarts",
-                NULL,
+            (systemd_metric[]){
+                {.name = "NRestarts", .type = "u"},
+                {.name = NULL},
             },
     },
     {.accounting_flag = NULL},
 };
+
+sd_bus *bus = NULL;
 
 // Err: negative
 static int get_prop(sd_bus *bus, char const type[static 1],
@@ -91,14 +99,9 @@ static int get_prop(sd_bus *bus, char const type[static 1],
   return r;
 }
 
-static int systemd_read(void) {
-  sd_bus *bus = NULL;
+static int systemd_read() {
+  int r;
   sd_bus_error sd_bus_err = SD_BUS_ERROR_NULL;
-  int r = sd_bus_open_system(&bus);
-  if (r < 0) {
-    ERROR("Failed to connect to system bus: %s", strerror(-r));
-    goto defer;
-  }
   for (systemd_accounting_group const *groups_it = groups;
        groups_it->accounting_flag != NULL; ++groups_it) {
     bool accounting_flag_var = true;
@@ -113,17 +116,18 @@ static int systemd_read(void) {
       }
     }
     if (accounting_flag_var) {
-      for (char **metrics_it = groups_it->metrics; *metrics_it != NULL;
-           ++metrics_it) {
+      for (systemd_metric *metrics_it = groups_it->metrics;
+           metrics_it->name != NULL; ++metrics_it) {
         uint64_t val;
-        r = get_prop(bus, "t", *metrics_it, &val, &sd_bus_err);
+        r = get_prop(bus, metrics_it->type, metrics_it->name, &val,
+                     &sd_bus_err);
         if (r < 0) {
-          ERROR("Failed to get %s property: %s {%s}, %s", *metrics_it,
+          ERROR("Failed to get %s property: %s {%s}, %s", metrics_it->name,
                 sd_bus_err.name, sd_bus_err.message, strerror(-r));
           goto defer;
         }
 
-        metric_family_t fam = {.name = *metrics_it,
+        metric_family_t fam = {.name = metrics_it->name,
                                .type = METRIC_TYPE_COUNTER};
         metric_family_metric_append(&fam, (metric_t){.value.counter = val});
         r = plugin_dispatch_metric_family(&fam);
@@ -137,10 +141,30 @@ static int systemd_read(void) {
   }
 
 defer:
-  sd_bus_unref(bus);
   sd_bus_error_free(&sd_bus_err);
 
   return EXIT_SUCCESS;
 }
 
-void module_register(void) { plugin_register_read("systemd", systemd_read); }
+static int systemd_init() {
+  if (bus == NULL) {
+    int r = sd_bus_open_system(&bus);
+    if (r < 0) {
+      ERROR("Failed to connect to system bus: %s", strerror(-r));
+      sd_bus_unref(bus);
+      return r;
+    }
+  }
+  return EXIT_SUCCESS;
+}
+
+static int systemd_shutdown() {
+  sd_bus_unref(bus);
+  return EXIT_SUCCESS;
+}
+
+void module_register() {
+  plugin_register_init("systemd", systemd_init);
+  plugin_register_read("systemd", systemd_read);
+  plugin_register_shutdown("systemd", systemd_shutdown);
+}
